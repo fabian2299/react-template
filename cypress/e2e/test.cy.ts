b8c6a697e506ddf48c0a86c8ff75d36786d95bab
fabian2299 <nievesfabian13@gmail.com>
2023-01-08 18:39:29 +0100

    fix types

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Goes to home and expect home string to exists', () => {
-    cy.visit('http://127.0.0.1:5173')
+    cy.visit('/')
     cy.findByRole('link', { name: /posts/i }).should('exist')
   })
 })
+
+export {}
