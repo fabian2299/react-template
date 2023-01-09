95b6d47e3b8052244bb01241752e4093e2fc809a
fabian2299 <nievesfabian13@gmail.com>
2023-01-09 21:26:43 +0100

    fetch from own api and added skeleton for crud posts operations

--- src/features/posts/components/delete-post/DeletePost.tsx
+++ src/features/posts/components/delete-post/DeletePost.tsx
@@ -1,27 +1,28 @@
+import { toast } from 'react-hot-toast'
 import { useDeletePostMutation } from '../../services/posts'
 import styles from './DeletePost.module.css'
 
 export default function DeletePost({ id }: { id: number }) {
-  const [deletePost, { isLoading }] = useDeletePostMutation()
+	const [deletePost, { isLoading }] = useDeletePostMutation()
 
-  const handleDelete = async () => {
-    try {
-      await deletePost(id.toString()).unwrap()
-      console.log('Post deleted')
-    } catch (error) {
-      console.log('Error deleting post')
-    }
-  }
+	const handleDelete = async () => {
+		try {
+			await deletePost(id).unwrap()
+			toast.success('Post deleted')
+		} catch (error) {
+			console.log('Error deleting post')
+		}
+	}
 
-  return (
-    <div>
-      <button
-        onClick={handleDelete}
-        disabled={isLoading}
-        className={styles['button']}
-      >
-        Delete Post
-      </button>
-    </div>
-  )
+	return (
+		<div>
+			<button
+				onClick={handleDelete}
+				disabled={isLoading}
+				className={styles['button']}
+			>
+				Delete Post
+			</button>
+		</div>
+	)
 }
--- src/features/posts/services/posts/index.ts
+++ src/features/posts/services/posts/index.ts
@@ -5,70 +5,70 @@ import { Post, PostDTO } from '../../../../types/post'
 
 // Define a service using a base URL and expected endpoints
 export const postApi = createApi({
-  reducerPath: 'postAPi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://jsonplaceholder.typicode.com/posts/',
-    // delay response by 2 second to simulate a slow server
-    fetchFn: async (args, init) => {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
-      return fetch(args, init)
-    },
-  }),
-  tagTypes: ['Post'],
-  endpoints: (builder) => ({
-    // Queries
-    getPostByID: builder.query<Post, string>({
-      query: (id) => id,
-    }),
-    getAllPosts: builder.query<Post[], void>({
-      query: () => '',
-      providesTags: (result, error, arg) =>
-        result
-          ? [...result.map(({ id }) => ({ type: 'Post' as const, id })), 'Post']
-          : ['Post'],
-    }),
-    // Mutations
-    createPost: builder.mutation<Post, PostDTO>({
-      query: (obj: PostDTO) => ({
-        url: '',
-        method: 'POST',
-        body: {
-          title: obj.title,
-          body: obj.body,
-          id: nanoid(),
-        },
-      }),
-      invalidatesTags: ['Post'],
-    }),
-    deletePost: builder.mutation<Post, string>({
-      query: (id) => ({
-        url: id,
-        method: 'DELETE',
-      }),
-      invalidatesTags: ['Post'],
-    }),
-    updatePost: builder.mutation<Post, PostDTO>({
-      query: (obj: PostDTO) => ({
-        url: obj.id?.toString() || '',
-        method: 'PATCH',
-        body: {
-          title: obj.title,
-          body: obj.body,
-          id: obj.id,
-          userId: obj.userId,
-        },
-      }),
-      invalidatesTags: ['Post'],
-    }),
-  }),
+	reducerPath: 'postAPi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://localhost:3000/api/posts/',
+		// delay response by 2 second to simulate a slow server
+		fetchFn: async (args, init) => {
+			await new Promise((resolve) => setTimeout(resolve, 2000))
+			return fetch(args, init)
+		},
+	}),
+	tagTypes: ['Post'],
+	endpoints: (builder) => ({
+		// Queries
+		getPostByID: builder.query<Post, string>({
+			query: (id) => id,
+		}),
+		getAllPosts: builder.query<Post[], void>({
+			query: () => '',
+			providesTags: (result, error, arg) =>
+				result
+					? [...result.map(({ id }) => ({ type: 'Post' as const, id })), 'Post']
+					: ['Post'],
+		}),
+		// Mutations
+		createPost: builder.mutation<Post, PostDTO>({
+			query: (obj: PostDTO) => ({
+				url: '',
+				method: 'POST',
+				body: {
+					title: obj.title,
+					body: obj.body,
+					id: nanoid(),
+				},
+			}),
+			invalidatesTags: ['Post'],
+		}),
+		deletePost: builder.mutation<Post, number>({
+			query: (id) => ({
+				url: `${id}`,
+				method: 'DELETE',
+			}),
+			invalidatesTags: ['Post'],
+		}),
+		updatePost: builder.mutation<Post, PostDTO>({
+			query: (obj: PostDTO) => ({
+				url: `${obj.id}`,
+				method: 'PATCH',
+				body: {
+					title: obj.title,
+					body: obj.body,
+					id: obj.id,
+					userId: obj.userId,
+				},
+			}),
+			invalidatesTags: ['Post'],
+		}),
+	}),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
 export const {
-  useGetPostByIDQuery,
-  useGetAllPostsQuery,
-  useCreatePostMutation,
-  useDeletePostMutation,
-  useUpdatePostMutation,
+	useGetPostByIDQuery,
+	useGetAllPostsQuery,
+	useCreatePostMutation,
+	useDeletePostMutation,
+	useUpdatePostMutation,
 } = postApi
