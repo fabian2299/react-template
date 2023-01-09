cfb7c7f2aa91bf349a79f40e6969546f32089b70
fabian2299 <nievesfabian13@gmail.com>
2023-01-09 19:28:43 +0100

    toast for post created added and test with cypress added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+	e2e: {
+		baseUrl: 'http://127.0.0.1:5173/',
+		viewportHeight: 1080,
+		viewportWidth: 1536,
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
 })
--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,15 @@
 describe('My First Test', () => {
 	it('Goes to home and expect home string to exists', () => {
-		cy.visit('/')
-		cy.findByRole('link', { name: /posts/i }).should('exist')
+		cy.visit('/posts')
+		// open create post modal
+		cy.findByRole('button', { name: /create post/i }).click()
+		// type to title input
+		cy.findByLabelText(/title/i).type('My first post')
+		// type to body input
+		cy.findByLabelText(/body/i).type('My first post body')
+		// click on submit button
+		cy.get('button[type="submit"]').click()
+		// post created toast should be visible
+		cy.findByText(/post created/i).should('exist')
 	})
 })
